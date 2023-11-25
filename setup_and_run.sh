6bf39813023d5b370a1f3548128c701de7884071
jtarrieu <jeremy.tarrieu@epfedu.fr>
2023-11-25 14:56:49 +0100

    working on install

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -6,7 +6,7 @@ install_docker () {
     sudo apt-get update
     sudo apt-get install ca-certificates curl gnupg
     sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     # Add the repository to Apt sources:
@@ -29,45 +29,10 @@ clone_repo () {
     git clone $repo_url -o $repo_dir
 }
 
-# CHECK IF DOCKER IS INSTALLED
-if command -v docker &> /dev/null ; then
-    echo "Docker is already installed."
-else
-    echo "Docker is not installed. Installing Docker..."
-    install_docker
-fi
-
-# CHECK IF DOCKER IS RUNNING
-if command -v docker info &> /dev/null ; then
-    echo "Docker is running."
-else
-    echo "Docker is not running. Starting Docker..."
-    start_docker_engine
-fi
-
-# CHECK IF GIT IS INSTALLED
-if command -v git &> /dev/null ; then
-    echo "Git is already installed."
-else
-    echo "Git is not installed. Installing Git..."
-    install_git
-fi
-
-# CHECK IF THE REPO HAS BEEN CLONED
-if [ -d "$repo_dir" ]; then
-    echo "Repository is already cloned."
-else
-    # CLONING THE DISCORD BOT REPO
-    clone_repo
-fi
-
-# CHECK IF DOCKER IS RUNNING
-if  &> /dev/null ; then
-    echo "Docker is running."
-else
-    echo "Docker is not running. Starting Docker..."
-    start_docker_engine
-fi
+install_docker
+start_docker_engine
+install_git
+clone_repo
 
 read -p "Enter your Discord bot token: " discord_token
 read -p "Enter your OpenAi token: " openai_token
